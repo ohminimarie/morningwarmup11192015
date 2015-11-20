43a28903cad67e2851c57d646dcc84b3efe41ea3
ohminimarie <mcjalcantara@gmail.com>
2015-11-20 10:28:57 -0800

    Morning warmup challenge for TH 11192015

---
+++ morningwarmup.js
@@ -0,0 +1,80 @@
+function Stream(arr) {
+
+  this.sum = function() {
+    var sum = 0;
+    for(var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+  }
+
+  this.median = function() {
+    var median;
+    if(arr.length % 2 === 0) {
+      var itemOne = arr[(arr.length / 2)];
+      var itemTwo = arr[(arr.length / 2) - 1];
+      median = (itemOne + itemTwo) / 2;
+    }
+    else {
+      median = arr[Math.floor(arr.length / 2)]
+    }
+  }
+
+  this.average = function() {
+    var avg = sum / arr.length;
+  }
+
+  this.streamPush = function(num) {
+
+  }
+}
+
+
+// Test code, though it might be useful to you.
+(function() {
+
+
+  var streamer = new Stream();
+  var arr = []
+  for(var i = 0; i < 100; i++) {
+    var current = Math.floor(Math.random() * 100);
+    streamer.streamPush(current);
+    arr.push(current);
+  }
+
+  // This is how we get those values for an array
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  // get the sum
+  var sum = 0;
+  for(var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  // use the sum to get the average
+  var avg = sum / arr.length;
+
+  // get the median
+  var median;
+  if(arr.length % 2 === 0) {
+    var itemOne = arr[(arr.length / 2)];
+    var itemTwo = arr[(arr.length / 2) - 1];
+    median = (itemOne + itemTwo) / 2;
+  }
+  else {
+    median = arr[Math.floor(arr.length / 2)]
+  }
+
+  console.log("--sum--");
+  console.log(sum);
+  console.log(streamer.sum());
+
+  console.log("\n--avg--");
+  console.log(avg);
+  console.log(streamer.average());
+
+  console.log("\n--median--");
+  console.log(median);
+  console.log(streamer.median());
+})();
